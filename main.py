bfc1b4c8f56e7987dff756967d03892bb167caba
UjjwalKandel2000 <ujjwalkandel2000@gmail.com>
2022-02-09 15:29:25 +0100

    updated streamlit web app

--- main.py
+++ main.py
@@ -27,21 +27,55 @@ col1, col2 = st.columns(2)
 # Checking the Format of the page
 if uploadFile is not None:
     # Perform your Manupilations (In my Case applying Filters)
-    img = load_image(uploadFile)
+    img_input = load_image(uploadFile)
     col1.header("Input Image")
-    col1.image(img)
-    col1.write(img.shape)
+    col1.image(img_input)
+    col1.write(img_input.shape)
+    cnn_model = load_model("models/preprocessed_data_cnn_model.h5")
     autoencoder = load_model('models/v0-1.h5')
-    pred = autoencoder.predict(img)
+    pred = autoencoder.predict(img_input)
+    
     col2.header("reconstructed image")
     pred = np.array(pred).reshape(128,128,1)
+    prediction = cnn_model.predict(np.array(img_input).reshape(1, 128, 128, 1))
     col2.image(pred)
-    col2.write(img.shape)
+    col2.write(pred.shape)
     COL1, COL2,COL3,COL4 = st.columns(4)
-    loss = SSIMLoss(img,pred)
+    loss = SSIMLoss(img_input,pred)
     label = 'SSIM Loss value: {:.3f}'
-
+    label_classes = 'Predicted Class: {}'
+    classes = np.argmax(prediction, axis = 1)
+    
     COL2.write(label.format(loss))
+    COL2.write(label_classes.format(classes[0]+1))
+
+    CNN_COL1, CNN_COL2 = st.columns(2)
+    img = Image.open(f"processed_data/templates/{classes[0]+1}.png")
+    CNN_COL1.header("input image")
+    CNN_COL1.image(img_input)
+    CNN_COL2.header("predicted_image")
+    CNN_COL2.image(img) 
+    img = np.array(img).reshape(128,128,1)
+    img = img/255
+
+    loss2 = SSIMLoss(img_input,img)
+    label = 'SSIM Loss value: {:.3f}'
+    label_classes = 'Predicted Class: {}'
+    COL1, COL2,COL3,COL4 = st.columns(4)
+    COL2.write(label.format(loss2))
+    label = 'Ratio loss values: {:.2f}'
+
+    COL2.write(label.format(loss/loss2))
+    result = ""
+    if loss/loss2 <0.70:
+        result = "anomaly"
+    else: result = "normal"
+    
+    COL2.write(result)
+
+
+    
+    
 
 
 else:
