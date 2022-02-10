3fec387a022228e0074ef006bd0147a433c382b5
manwithplan <aubin.jan.huet@gmail.com>
2022-02-10 10:13:49 +0100

    updating input of numpy model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PIL import Image
-import numpy as np 
+import numpy as np
 import streamlit as st
 from keras.models import load_model
 import tensorflow as tf
@@ -90,7 +90,7 @@ def match_template(
         return mse, None, None
 
 
-def numpy_model(image, path=False, thresholds=None):
+def numpy_model(image, path=True, thresholds=None):
     """
     predicts whether a dice is an anomaly or not
     :image: str path to image file
@@ -132,23 +132,31 @@ def numpy_model(image, path=False, thresholds=None):
 
     return predicted, predictions
 
+
 def SSIMLoss(y_true, y_pred):
-  y_true = tf.cast(y_true,tf.float32)
-  y_pred = tf.cast(y_pred,tf.float32)
-  ssimloss = 1 - tf.reduce_mean(tf.image.ssim(y_true, y_pred,1.0))
-  return ssimloss
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(y_pred, tf.float32)
+    ssimloss = 1 - tf.reduce_mean(tf.image.ssim(y_true, y_pred, 1.0))
+    return ssimloss
+
 
 # Function to Read and Manupilate Images
 def load_image(img):
     im = Image.open(img)
     image = np.array(im)
-    image = image.reshape(1,128,128,1)
-    image = image/255
+    image = image.reshape(1, 128, 128, 1)
+    image = image / 255
 
     return image[0]
 
+
+def load_image_opencv(img):
+    image = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+    return image
+
+
 # Uploading the File to the Page
-uploadFile = st.file_uploader(label="Upload image", type=['jpg', 'png'])
+uploadFile = st.file_uploader(label="Upload image", type=["jpg", "png"])
 col1, col2 = st.columns(2)
 
 # Checking the Format of the page
@@ -157,60 +165,58 @@ if uploadFile is not None:
 
     # Perform your Manupilations (In my Case applying Filters)
     img_input = load_image(uploadFile)
+    img_opencv = load_image_opencv(uploadFile)
     col1.header("Input Image")
     col1.image(img_input)
     col1.write(img_input.shape)
     cnn_model = load_model("models/preprocessed_data_cnn_model.h5")
-    autoencoder = load_model('models/v0-1.h5')
+    autoencoder = load_model("models/v0-1.h5")
     pred = autoencoder.predict(img_input)
-    
+
     col2.header("reconstructed image")
-    pred = np.array(pred).reshape(128,128,1)
+    pred = np.array(pred).reshape(128, 128, 1)
     prediction = cnn_model.predict(np.array(img_input).reshape(1, 128, 128, 1))
     col2.image(pred)
     col2.write(pred.shape)
-    COL1, COL2,COL3,COL4 = st.columns(4)
-    loss = SSIMLoss(img_input,pred)
-    label = 'SSIM Loss value: {:.3f}'
-    label_classes = 'Predicted Class: {}'
-    classes = np.argmax(prediction, axis = 1)
-    
+    COL1, COL2, COL3, COL4 = st.columns(4)
+    loss = SSIMLoss(img_input, pred)
+    label = "SSIM Loss value: {:.3f}"
+    label_classes = "Predicted Class: {}"
+    classes = np.argmax(prediction, axis=1)
+
     COL2.write(label.format(loss))
-    COL2.write(label_classes.format(classes[0]+1))
+    COL2.write(label_classes.format(classes[0] + 1))
 
     CNN_COL1, CNN_COL2 = st.columns(2)
     img = Image.open(f"processed_data/templates/{classes[0]+1}.png")
     CNN_COL1.header("input image")
     CNN_COL1.image(img_input)
     CNN_COL2.header("predicted_image")
-    CNN_COL2.image(img) 
-    img = np.array(img).reshape(128,128,1)
-    img = img/255
+    CNN_COL2.image(img)
+    img = np.array(img).reshape(128, 128, 1)
+    img = img / 255
 
-    loss2 = SSIMLoss(img_input,img)
-    label = 'SSIM Loss value: {:.3f}'
-    label_classes = 'Predicted Class: {}'
-    COL1, COL2,COL3,COL4 = st.columns(4)
+    loss2 = SSIMLoss(img_input, img)
+    label = "SSIM Loss value: {:.3f}"
+    label_classes = "Predicted Class: {}"
+    COL1, COL2, COL3, COL4 = st.columns(4)
     COL2.write(label.format(loss2))
-    label = 'Ratio loss values: {:.2f}'
+    label = "Ratio loss values: {:.2f}"
 
-    COL2.write(label.format(loss/loss2))
+    COL2.write(label.format(loss / loss2))
     result = ""
-    if loss/loss2 <0.70:
+    if loss / loss2 < 0.70:
         result = "anomaly"
-    else: result = "normal"
-    
+    else:
+        result = "normal"
+
     COL2.write(result)
 
-    np_model = numpy_model(img_input)
-    st.write("loaded numpy model")
+    np_prediction = numpy_model(img_opencv, path=False)
+    st.write("predicting with numpy model")
 
-    st.write(np_model)
+    st.write(np_prediction[0])  # returns 0 for normal and 1 for anomaly
 
-    
+
 else:
     st.write("Make sure you image is in JPG/PNG Format.")
-
-
- 
-
